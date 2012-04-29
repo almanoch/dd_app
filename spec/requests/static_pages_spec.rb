0362dabd93faa42a682f94f0b1df119602820512
Anatoliy Khan <aktee@mail.ru>
2012-04-28 18:30:39 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+    
+    it "should have the h1 'Dirdem App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Dirdem App')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                  :text => "Direct Democracy Dirdem App | Home")
+    end
+  end
+  
+  describe "Help page" do
+    
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                  :text => "Direct Democracy Dirdem App | Help")
+    end
+  end
+  
+  describe "About Us" do
+    
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                  :text => "Direct Democracy Dirdem App | About Us")
+    end
+  end
+end
